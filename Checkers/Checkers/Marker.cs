d4a36633cc20cf1f82dda1d63de810b1f6d38e9f
Steven Gibson <s-gibson89@live.co.uk>
2017-10-06 22:07:35 +0100

    Restarted Project
    tidied up code to make the board to allow future updates also added classes

---
+++ Checkers/Checkers/Marker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    class Marker
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Marker(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+
+        public override bool Equals(System.Object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            Marker marker = obj as Marker;
+            if ((System.Object)marker ==null)
+            {
+                return false;
+            }
+
+            return Row == marker.Row && Column == marker.Column;
+        }
+
+
+    }
+}
